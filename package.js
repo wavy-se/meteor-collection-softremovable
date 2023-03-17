08034da3310be3fed424bde6ce7ea447c8baafdd
Robin Jakobsson <jakobsson.robin@gmail.com>
2023-03-17 10:22:01 +0100

    Update package.js
    bump dependencies

--- package.js
+++ package.js
@@ -15,14 +15,14 @@ Package.onUse(function(api) {
   ]);
 
   api.use([
-    'matb33:collection-hooks@0.9.0-rc.1',
-    'zimme:collection-behaviours@1.0.3'
+    'matb33:collection-hooks',
+    'zimme:collection-behaviours'
   ]);
 
   api.use([
     'aldeed:autoform@4.0.0 || 5.0.0',
     'aldeed:collection2@2.0.0',
-    'aldeed:simple-schema@1.0.3'
+    'aldeed:simple-schema'
   ], ['client', 'server'], {weak: true});
 
   api.imply('zimme:collection-behaviours');
